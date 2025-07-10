194889da91ad9ea8b2f10766871776b74cb108e9
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-07-10 15:56:04 +0600

    login functionality added

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
 import axiosInstance from "@/src/lib/AxiosInstance";
 
@@ -18,4 +19,46 @@ export const registerUser = async (userData: FieldValues) => {
   } catch (error: any) {
     throw new Error(error);
   }
+};
+
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      (await cookies()).set("accessToken", data?.data?.accessToken);
+      (await cookies()).set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const logout = async () => {
+  (await cookies()).delete("accessToken");
+  (await cookies()).delete("refreshToken");
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")?.value;
+
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+
+    return {
+      _id: decodedToken._id,
+      name: decodedToken.name,
+      email: decodedToken.email,
+      mobileNumber: decodedToken.mobileNumber,
+      role: decodedToken.role,
+      status: decodedToken.status,
+      profilePhoto: decodedToken.profilePhoto,
+    };
+  }
+
+  return decodedToken;
 };
