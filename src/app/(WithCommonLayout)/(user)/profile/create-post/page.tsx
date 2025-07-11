4e95495d51b35a8e4e658042d515cea0bf7e50b4
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-07-11 23:49:56 +0600

    create page funtionality added

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+"use client";
 
-const page = () => {
+import { Divider } from "@heroui/divider";
+import { Button } from "@heroui/button";
+import {
+    FieldValues,
+    FormProvider,
+    SubmitHandler,
+    useFieldArray,
+    useForm,
+} from "react-hook-form";
+
+import FXInput from "@/src/components/form/FXInput";
+
+export default function page() {
+    const methods = useForm();
+
+    const { control, handleSubmit } = methods;
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "questions",
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        const postData = {
+            ...data,
+            questions: data.questions.map((que: { value: string }) => que.value),
+        };
+
+        console.log(postData);
+    };
+
+    const handleFieldAppend = () => {
+        append({ name: "questions" });
+    };
+
     return (
-        <div>page</div>
-    )
-}
+        <div>
+            <FormProvider {...methods}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FXInput label="Title" name="title" />
 
-export default page
+                    <Divider className="my-5" />
+
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-xl">Owner verification questions</h1>
+                        <Button onClick={() => handleFieldAppend()}>Append</Button>
+                    </div>
+
+                    {fields.map((field, index) => (
+                        <div key={field.id} className="flex items-center">
+                            <FXInput label="Question" name={`questions.${index}.value`} />
+                            <Button onClick={() => remove(index)}>Remove</Button>
+                        </div>
+                    ))}
+
+                    <Divider className="my-5" />
+
+                    <Button type="submit">Post</Button>
+                </form>
+            </FormProvider>
+        </div>
+    );
+}
